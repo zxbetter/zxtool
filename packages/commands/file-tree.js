f11180eacc97927bc8626066427653947bc593a7
devin <itdevin@163.com>
2019-05-13 11:47:18 +0800

    feat(file-tree): 目录树先展示目录，再展示文件

--- packages/commands/file-tree.js
+++ packages/commands/file-tree.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const fs = require('fs')
+const chalk = require('chalk')
 
 const BRANCH = '├─',
   LAST_BRANCH = '└─',
@@ -12,13 +13,27 @@ const excludes = []
 exports.exec = function (dirs, options) {
   // 指定排除参数
   if (options.exclude && options.exclude.length) {
-    options.exclude.forEach(function(item) {
+    options.exclude.forEach(function (item) {
       excludes.push(new RegExp(item))
     })
   }
 
-  dirs.forEach(function (dir) {
-    showDirTree(dir, '')
+  const groupObj = groupDirs('', dirs)
+
+  // 处理入参中的目录
+  groupObj.dir.forEach(dir => {
+    console.log(`${chalk.cyan(path.resolve(dir))} 处理结果：`)
+    showDirTree(process.cwd() === path.resolve(dir) ? '.' : path.basename(path.resolve(dir)), '')
+  })
+
+  // 处理入参中的文件
+  groupObj.file.forEach(file => {
+    const fileName = path.basename(path.resolve(file))
+
+    if (validExclude(fileName)) {
+      console.log(`${chalk.cyan(path.resolve(file))} 处理结果：`)
+      console.log(`${LAST_BRANCH}${fileName}`)
+    }
   })
 }
 
@@ -30,16 +45,6 @@ exports.exec = function (dirs, options) {
  */
 function showDirTree(dirName, palceHolder) {
   const dirPath = path.resolve(dirName)
-  const dirStat = fs.statSync(dirPath)
-  // 判断是否是目录
-  if (!dirStat.isDirectory()) {
-    // 命令参数中指定的不是目录，则直接输出。
-    if (palceHolder === '' && validExclude(dirName)) {
-      console.log(`${palceHolder}${LAST_BRANCH}${dirName}`)
-    }
-    return
-  }
-
   const files = fs.readdirSync(dirPath)
   // 防止空的子目录重复输出。
   if (!files.length) {
@@ -50,26 +55,26 @@ function showDirTree(dirName, palceHolder) {
     return
   }
 
-  const validatedFiles = files.filter(function(file) {
+  const validatedFiles = files.filter(function (file) {
     return validExclude(file)
   })
 
-  // 遍历目录
-  for (let i = 0; i < validatedFiles.length; i++) {
-    const file = validatedFiles[i]
-    const branch = i === validatedFiles.length - 1 ? LAST_BRANCH : BRANCH
-    if (validExclude(file)) {
-      console.log(`${palceHolder}${branch}${file}`)
-    }
+  const groupObj = groupDirs(dirPath, validatedFiles)
 
-    const filePath = path.join(dirPath, file)
-    const fileStat = fs.statSync(filePath)
-    if (fileStat.isDirectory()) {
-      showDirTree(filePath,
-        (palceHolder === '' && branch !== LAST_BRANCH) ?
-        TAB :
-        `${palceHolder}${(branch === LAST_BRANCH ? EMPTY_TAB : TAB)}`)
-    }
+  // 先输出目录
+  for (let i = 0, childDirs = groupObj.dir; i < childDirs.length; i++) {
+    let dirName = childDirs[i]
+    let branch = i === validatedFiles.length - 1 ? LAST_BRANCH : BRANCH
+    console.log(`${palceHolder}${branch}${dirName}`)
+    showDirTree(path.join(dirPath, dirName),
+      (palceHolder === '' && branch !== LAST_BRANCH) ?
+      TAB :
+      `${palceHolder}${(branch === LAST_BRANCH ? EMPTY_TAB : TAB)}`)
+  }
+
+  // 再输出文件
+  for (let i = 0, childFiles = groupObj.file; i < childFiles.length; i++) {
+    console.log(`${palceHolder}${i === childFiles.length - 1 ? LAST_BRANCH : BRANCH}${childFiles[i]}`)
   }
 }
 
@@ -80,4 +85,36 @@ function validExclude(name) {
     }
   }
   return true
+}
+
+/**
+ * 把目录和文件分开
+ * 
+ * @param root 根目录
+ * @param dirs 待分组的文件
+ * @returns 分组后的结果
+ */
+function groupDirs(root, dirs) {
+  let result = {
+    dir: [],
+    file: []
+  }
+
+  if (!dirs || !dirs.length) return result;
+
+  for (let i = 0; i < dirs.length; i++) {
+    let dir = dirs[i]
+    let filePath = path.resolve(root, dir)
+    if (!fs.existsSync(filePath)) {
+      console.log(chalk.red(`${filePath} 文件不存在`))
+      continue
+    }
+    if (!fs.statSync(path.resolve(root, dir)).isDirectory()) {
+      result.file.push(dir)
+    } else {
+      result.dir.push(dir)
+    }
+  }
+
+  return result
 }
